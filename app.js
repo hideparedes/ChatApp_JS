1ed8d72e8747b1864f9cc59b7b9e74bd14fd6dc6
Hidenori Kawano <hidenori.kawano@gmail.com>
2019-11-29 23:23:25 +0900

    Add username prompt

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const io = require("socket.io")(server);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const today = new Date();
+const options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric"
+};
 
+const users = {};
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    date: today.toLocaleDateString("en-US", options)
+  });
 });
 
 io.on("connection", socket => {
-  socket.on('send message', message => {
-    socket.broadcast.emit('display message', message)
+  socket.on("new user", name => {
+    users[socket.id] = name;
+    socket.broadcast.emit("connecter", name);
+  });
 
+  socket.on("send message", message => {
+    socket.broadcast.emit("display message", message);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected", users[socket.id]);
+    delete users[socket.id];
   });
 });
 
--- public/index.js
+++ public/index.js
@@ -1,25 +1,33 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container');
-const messageForm = document.getElementById('send-container');
-const inputMessage = document.getElementById('input-message');
+const socket = io("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const inputMessage = document.getElementById("input-message");
 
+const name = prompt("Enter your username");
+socket.emit("new user", name);
+
+socket.on("connecter", name => {
+  addToChat(`${name} is connected`);
+});
+
+socket.on("user disconnected", name => {
+  addToChat(`${name} has left the chat`);
+});
 
 messageForm.addEventListener("submit", event => {
   event.preventDefault();
   const message = inputMessage.value;
   addToChat(`You: ${message}`);
-  socket.emit('send message', message);
-  inputMessage.value = '';
+  socket.emit("send message", message);
+  inputMessage.value = "";
 });
 
-socket.on('display message', data => {
-
+socket.on("display message", data => {
   addToChat(data);
 });
 
-
 function addToChat(message) {
-  const messageElement = document.createElement('P');
+  const messageElement = document.createElement("P");
   messageElement.innerText = message;
   messageContainer.append(messageElement);
 }
