48cbf6737f6f87a9a85af7dca764028322de6de4
Hidenori Kawano <hidenori.kawano@gmail.com>
2019-12-10 22:37:16 +0900

    Fixed scroll issue

--- public/index.js
+++ public/index.js
@@ -1,16 +1,14 @@
-const socket = io('http://localhost:3000')
+const socket = io("http://localhost:3000");
 
-document.getElementById("username-form").addEventListener("submit", (event) => {
+document.getElementById("username-form").addEventListener("submit", event => {
   event.preventDefault();
   const username = document.getElementById("username");
   socket.emit("username", username.value);
 });
 
-
 //---------------------------------------------
 const chatWindow = document.getElementById("chat-window");
 const onlineList = document.getElementById("online-list");
-
 
 socket.on("online", username => {
   checkUserStatus(username, "is connected");
@@ -26,19 +24,16 @@ socket.on("user disconnected", username => {
   checkUserStatus(username, "is disconnected");
 });
 
-
 function checkUserStatus(name, status) {
   const messageCard = document.createElement("div");
   messageCard.classList.add("message-card", "from-others");
   messageCard.id = "message-card";
   const content = document.createElement("div");
   content.innerText = name + " " + status;
-  content.classList.add("content")
+  content.classList.add("content");
   messageCard.append(content);
   chatWindow.append(messageCard);
 }
-
-
 
 function onlineUsers(name) {
   const user = document.createElement("div");
@@ -46,7 +41,6 @@ function onlineUsers(name) {
   user.id = "friends";
   onlineList.append(user);
 }
-
 
 const messageForm = document.getElementById("send-container");
 const inputMessage = document.getElementById("input-message");
@@ -59,8 +53,6 @@ messageForm.addEventListener("submit", event => {
   inputMessage.value = "";
 });
 
-
-
 function addYourMessage(message) {
   const messageCard = document.createElement("div");
   messageCard.classList.add("message-card", "from-you");
@@ -72,11 +64,10 @@ function addYourMessage(message) {
   chatWindow.append(messageCard);
 }
 
-
 function addToChatWindow(message, user) {
   const messageCard = document.createElement("div");
   messageCard.classList.add("message-card", "from-others");
-  const userName = document.createElement("div");
+  const userName = document.createElement("p");
   userName.innerText = user;
   const content = document.createElement("div");
   content.innerText = message;
@@ -85,4 +76,15 @@ function addToChatWindow(message, user) {
   messageCard.append(content);
 
   chatWindow.append(messageCard);
+}
+
+const observer = new MutationObserver(scrollToBottom);
+
+const config = {
+  childList: true
+};
+observer.observe(chatWindow, config);
+
+function scrollToBottom() {
+  chatWindow.scrollTop = chatWindow.scrollHeight;
 }
